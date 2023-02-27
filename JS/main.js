79064714c743af41d8d45f24c7439540455e61ec
my name <my@email.com>
2023-02-27 21:53:03 +0500

    JS added

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+function darkmode() {
+  document.getElementById("headerr").style.backgroundColor = "black";
+  document.getElementById("wrapper1.1").style.backgroundColor = "black";
+  document.getElementById("wrapper2.2").style.backgroundColor = "black";
+  document.getElementById("wrapper3.3").style.backgroundColor = "black";
+  document.getElementById("wrapper4.4").style.backgroundColor = "black";
+  document.getElementById("wrapper5.5").style.backgroundColor = "black";
+  document.getElementById("wrapper6.6").style.backgroundColor = "black";
+}
+function lightmode() {
+  document.getElementById("headerr").style.backgroundColor = "white";
+  document.getElementById("wrapper1.1").style.backgroundColor = "white";
+  document.getElementById("wrapper2.2").style.backgroundColor = "white";
+  document.getElementById("wrapper3.3").style.backgroundColor = "white";
+  document.getElementById("wrapper4.4").style.backgroundColor = "white";
+  document.getElementById("wrapper5.5").style.backgroundColor = "white";
+  document.getElementById("wrapper6.6").style.backgroundColor = "white";
+}
